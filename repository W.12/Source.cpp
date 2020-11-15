9acc043cd3138c7dc8eeb8a3235d00b1cdaf7350
PASIN KEAWNIL <63010656@KMITL.AC.TH>
2020-11-15 23:41:47 +0700

    add new

--- repository W.12/Source.cpp
+++ repository W.12/Source.cpp
@@ -3,80 +3,40 @@
 #include <algorithm>
 #include <math.h>
 using namespace std;
+
 struct kk
 {
-    char c;
-    int p = 0;
+    int x, y;
+    char top[129], bot[129], key[129];
 };
-
-void _swap(kk& x, kk& y)
+template <typename T>
+void _swap(T& x, T& y)
 {
-    kk t = x;
+    T temp = x;
     x = y;
-    y = t;
-}
-void _sort(kk data[], int live)
-{
-
-    int v = 0;
-    for (int i = live - 1; i >= 0; i--)
-    {
-        for (int j = 0; j < i; j++)
-        {
-            if (data[j].p < data[j + 1].p)
-                _swap(data[j], data[j + 1]);
-        }
-    }
+    y = temp;
 }
 int main()
 {
-    kk data[8 + 1];
-    int live = 7;
-    int still = 7;
-    for (int i = 0; i < 7; i++)
-        data[i].c = char('A' + i);
-    string k = "0";
-    int pl = 6;
-    while (k[0] != '!')
-    {
-        cin >> k;
-        if (k[0] == '!')
-            break;
-        for (int i = 0; i < k.size(); i++)
-        {
-            int v = k[i] - 'A';
-            if (data[v].p != -1)
-                data[v].p++;
-        }
-        if (still != 1)
-        {
 
-            int min = 2e10, v = 0;
-            for (int i = 0; i < live; i++)
-                if (data[i].p < min and data[i].p != -1)
-                    min = data[i].p;
-            for (int i = 0; i < live; i++)
-                if (data[i].p == min)
-                    v++;
-            if (v == 1)
-                for (int i = 0; i < live; i++)
-                    if (data[i].p == min)
-                    {
-                        data[i].p = -1;
-                        still--;
-                    }
+    kk data;
+    cin >> data.x >> data.y >> data.top >> data.bot >> data.key;
+    for (int i = 0; i < data.x; i++)
+        if (data.top[i] > data.bot[i])
+            _swap(data.top[i], data.bot[i]);
+
+    for (int i = 0; i < data.y; i++)
+    {
+        for (int j = 0; j < data.x; j++)
+        {
+            if (data.key[i] > data.bot[j])
+                data.key[i] = data.bot[j];
+
+            else if (data.key[i] < data.top[j])
+                data.key[i] = data.top[j];
         }
     }
-skip:;
+    printf("%s", data.key);
 
-    _sort(data, live);
 
-    int v = 0;
-    for (int i = 0; i < 7; i++)
-        if (data[i].p != -1)
-            v++;
-    cout << v << endl;
-    for (int i = 0; i < live; i++)
-        if (data[i].p != -1)
-            cout << data[i].c << " " << data[i].p << endl;
 }
